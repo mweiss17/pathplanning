50f8784ff8110aa580816bf6962afea806c074b3
Vincent Mai <vincent.mai@umontreal.ca>
2018-12-17 17:02:37 -0500

    Regularize agent parameters

--- src/dt_agent_node.py
+++ src/dt_agent_node.py
@@ -8,22 +8,29 @@ from dt_agent.agent import Agent
 
 class AgentNode(object):
     def __init__(self):  
+
         # Parameters 
-        self.dt = rospy.get_param("/dt_agent_node/dt")
-        self.roadwidth = rospy.get_param("/dt_agent_node/road/width")
-        self.horizon_length = rospy.get_param("/dt_agent_node/road/horizon_length")
+
+        ## Time
+        self.dt = rospy.get_param("/sim/dt")
+
+        ## World parameters
+        self.road_width = rospy.get_param("/sim/world/road/width")
+
+        ## Agent parameters
+        self.horizon_length = rospy.get_param("/agent/horizon_length")
+        
 
         #Publishers
         self.pub_agent_orientation_seq = rospy.Publisher("/agent/orientation_seq",Float32MultiArray, queue_size=1)
         self.pub_agent_computation_time_steps = rospy.Publisher("/agent/computation_time_steps", Int32, queue_size=1)
         
-
         # Subscribers
         self.sub_pose_other_duckie_obs = rospy.Subscriber("/sim/obs/pose_other_duckie",Pose2D, self.compute_ourplan_cb)
         
         self.othduckie_obs = Pose2D()
         self.received_othduckie_obs = False
-        self.agent = Agent(self.roadwidth, self.horizon_length)
+        self.agent = Agent(self.road_width, self.horizon_length)
 
 def compute_ourplan_cb(self, othduckie_obs_msg):   
     rospy.loginfo("[SimNode] Received other duckie's obs.")
